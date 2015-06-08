dc06d8496a9542f539445f37809adbda50c5c5f4
Dino Morelli <dino@ui3.info>
2015-06-08 19:41:54 +0000

    Added more vertical whitespace

--- src/FlavoursMVar/Lock.hs
+++ src/FlavoursMVar/Lock.hs
@@ -3,10 +3,13 @@ module FlavoursMVar.Lock
 
 import Control.Concurrent.MVar
 
+
 type Lock = MVar ()
+
 
 newLock :: IO Lock
 newLock = newMVar ()
+
 
 withLock :: Lock -> IO a -> IO a
 withLock x = withMVar x . const
